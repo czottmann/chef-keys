b7c009bbd75bb611ccb8f75f4ee459e4b2381b5f
Cody Dracars <cdracars@gmail.com>
2013-03-20 16:08:51 -0500

    fixed for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name              "keys"
 maintainer        "Carlo Zottmann"
 maintainer_email  "carlo@zottmann.org"
 license           "WTFPL"
 description       "Creates keys in /home/chef/.ssh/ from a data bag"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0"
 recipe            "keys", "Copies keys from data bag."
 
